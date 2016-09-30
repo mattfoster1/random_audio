5129e2b8240eabc20478bdb93446432a54f43fcc
M Foster <matt@mfoster.co.uk>
2016-09-30 20:17:38 +0100

    first gen product. Needs visual improvements

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ var beat = 100;
 var amber_boundary = Math.floor((window.innerHeight * 1) / 2);
 var red_boundary = Math.floor((window.innerHeight * 2) / 3);
 var setup = 1;
+var totalColumnWidth = 30;
 
 var start = function() {
+	setup();
 	alterations();
 	sizing();
 	// alterations();
-	// main = setInterval(function(){ sizing(); }, beat); // this needs to be randomised for the beat
+	main = setInterval(function(){ sizing(); }, beat); // this needs to be randomised for the beat
 
 	if (window.innerHeight > window.innerWidth) {
 		orientation = "portrait";
@@ -26,9 +28,31 @@ var alterations = function() {
 	redPart = document.getElementsByClassName("redPart");
 }
 
-var sizing = function() { //used to calculate height
-	for (n=0; n < greenPart.length; n++) {
+var setup = function() {
+	var totalColumns = Math.floor(window.innerWidth / totalColumnWidth);
 
+	for (x=1; x < totalColumns; x++) {
+		var newDiv = document.createElement("div");
+		$(newDiv).addClass("mf_column").addClass("column" + x);
+		document.getElementsByClassName("cont1")[0].appendChild(newDiv);
+		
+		var newChildGreen = document.createElement("div");
+		$(newChildGreen).addClass("greenPart").addClass("segment");
+		newDiv.appendChild(newChildGreen);
+
+		var newChildAmber = document.createElement("div");
+		$(newChildAmber).addClass("amberPart").addClass("segment");
+		newDiv.appendChild(newChildAmber);
+
+		var newChildRed = document.createElement("div");
+		$(newChildRed).addClass("redPart").addClass("segment");
+		newDiv.appendChild(newChildRed);
+	}
+}
+
+var sizing = function() { //used to calculate height
+
+	for (n=0; n < greenPart.length; n++) {
 		if (n >= 2) { // correlates height with previous two columns' height
 			var oneColBehind = parseInt(total_height[(n-1)]);
 			var twoColBehind = parseInt(total_height[(n-2)]);
@@ -47,5 +71,5 @@ var sizing = function() { //used to calculate height
 // Tasks: 
 
 // 1. set a totalColumnWidth for each column. Make is an easily changed var
-// 2. divide totatColumnWidth by window.innerWidth to get total number of columns
+// 2. divide totalColumnWidth by window.innerWidth to get total number of columns
 // 3. use a loop with totalColumns.length as limit to generate new columns
